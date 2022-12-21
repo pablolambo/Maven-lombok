ca384e24e0be784e1eb7a39ce328141d54a1481c
pablolambo <pawelfrankowski42@gmail.com>
2022-12-21 17:47:27 +0100

    Maven added
    log4j2 dependence added

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new Vehicle();
+        vehicle1.printCos();
+    }
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,9 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Vehicle {
+    void printCos(){
+        System.out.println("cos");
+    }
+}
