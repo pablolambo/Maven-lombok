9c6bac18d534c6a0719230bb35b2b0170ec81647
pablolambo <pawelfrankowski42@gmail.com>
2022-12-22 16:41:41 +0100

    Switching branches

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Vehicle vehicle1 = new Vehicle();
-        vehicle1.printCos();
+        MainLoop mainLoop = new MainLoop();
+        mainLoop.optionsLoop();
     }
 }
